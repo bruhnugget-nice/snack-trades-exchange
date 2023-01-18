0bbe4cef8387f0f501b2320d3e16e0c1170b20e3
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2023-01-17 20:08:21 -0500

    refactor: more dynamic

--- js/main.js
+++ js/main.js
@@ -1,97 +1,102 @@
 //THEME DRAMA
-        //Gets the theme button element.
-        var themeButton = document.getElementById("themeChanger");
-        var headingDiv = document.getElementById("heading");
-        //The function given to the button for updating the theme.
-        var onThemeButtonClick = function() {
-            var theme = document.getElementById("theme-picker").value;
-            if (theme === "light") {
-                alert("Look, I don't know why you would do this, but just to tell you, you really should switch over to the dark side.");
-                document.body.style.backgroundColor = "#dedede";
-                document.body.style.color = "#121212";
-                headingDiv.style.backgroundColor = "#a60000";
-            } else if (theme === "dark") {
-                alert("Finally, inner peace.");
-                document.body.style.backgroundColor = "#121212";
-                document.body.style.color = "#dedede";
-                headingDiv.style.backgroundColor = "#a60000";
-            } else if (theme === "tabularasa") {
-                alert("John Locke.mp4");
-                document.body.style.backgroundColor = "#121212";
-                document.body.style.color = "#121212";
-                headingDiv.style.backgroundColor = "#121212";
-            } else if (theme === "void") {
-                alert("Void");
-                document.body.style.backgroundColor = "#dedede";
-                document.body.style.color = "#dedede";
-                headingDiv.style.backgroundColor = "#dedede";
-            } else {
-                alert("...");
-            }
-        }
-        //Gives the button the onThemeButtonClick function.
-        themeButton.addEventListener("click", onThemeButtonClick);
-        //OTHER STUFF
-        var convertButton = document.getElementById("convertButton");
-        var output = document.getElementById("results");
-        //function for convert
-        var onConvertButtonClick = function() {
-            var snackFrom = document.getElementById("snackFrom").value;
-            var amount = document.getElementById("amount").value;
-            var snackTo = document.getElementById("snackTo").value;
-            if (snackFrom === "oreo" && snackTo === "takiTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt * 3;
-                output.innerHTML = amountToInt + " Oreos are/is equal to " + newAmount + " Taki(s).";
-            } else if ((snackFrom==="oreo" && snackTo==="oreoTo") || (snackFrom==="taki" && snackTo==="takiTo") || (snackFrom==="twinkie" && snackTo==="twinkieTo") || (snackFrom==="welch" && snackTo==="welchTo")) {
-                output.innerHTML = "Look, they are the same amount.";
-            } else if (snackFrom === "oreo" && snackTo === "twinkieTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt/5;
-                output.innerHTML = amountToInt + " Oreos are/is equal to " + newAmount + " Twinkie(s).";
-            } else if (snackFrom === "oreo" && snackTo === "welchTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt/5;
-                output.innerHTML = amountToInt + " Oreos are/is equal to " + newAmount + " Welch's Fruit Gummies.(in packet/packets)";
-            } else if (snackFrom === "taki" && snackTo === "oreoTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt/3;
-                output.innerHTML = amountToInt + " Taki are/is equal to " + newAmount + " Oreo(s)";       
-            } else if (snackFrom === "taki" && snackTo === "twinkieTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt/15;
-                output.innerHTML = amountToInt + " Taki are/is equal to " + newAmount + " Twinkie(s)";       
-            } else if (snackFrom === "taki" && snackTo === "welchTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt/15;
-                output.innerHTML = amountToInt + " Taki are/is equal to " + newAmount + " Welch's Fruit Gummies.(in packet/packets)";       
-            } else if (snackFrom === "twinkie" && snackTo === "oreoTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt*3;
-                output.innerHTML = amountToInt + " Twinkie(s) are/is equal to " + newAmount + " Oreo(s)";    
-            } else if (snackFrom === "twinkie" && snackTo === "takiTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt*15;
-                output.innerHTML = amountToInt + " Twinkie(s) are/is equal to " + newAmount + "Taki(s).";    
-            } else if (snackFrom === "twinkie" && snackTo === "welchTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt*1;
-                output.innerHTML = amountToInt + " Twinkie(s) are/is equal to " + newAmount + " Welch's Fruit Gummies.(in packet/packets)";    
-            } else if (snackFrom === "welch" && snackTo === "oreoTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt*5;
-                output.innerHTML = amountToInt + " Welch's Fruit Gummies.(in packet/packets) " + newAmount + " Oreo(s)";    
-            } else if (snackFrom === "welch" && snackTo === "takiTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt*15;
-                output.innerHTML = amountToInt + " Welch's Fruit Gummies.(in packet/packets) " + newAmount + "Taki(s).";    
-            } else if (snackFrom === "welch" && snackTo === "twinkieTo") {
-                var amountToInt = parseInt(amount);
-                var newAmount = amountToInt*1;
-                output.innerHTML = amountToInt + " Welch's Fruit Gummies.(in packet/packets) " + newAmount + " Twinkie(s)";    
-            }else{
-                    
-            }
-        }
+//Gets the theme button element.
+let themeButton = document.getElementById("themeChanger");
+let headingDiv = document.getElementById("heading");
+//Object for storing theme information
+const themes = {
+  light: {
+    body: "#dedede",
+    text: "#121212",
+    header: "#a60000",
+    description:
+      "Look, I don't know why you would do this, but just to tell you, you really should switch over to the dark side.",
+  },
+  dark: {
+    body: "#121212",
+    text: "#dedede",
+    header: "#a60000",
+    description: "Finally, inner peace.",
+  },
+  tabularasa: {
+    body: "#121212",
+    text: "#121212",
+    header: "#121212",
+    description: "John Locke.mp4",
+  },
+  void: {
+    body: "#dedede",
+    text: "#dedede",
+    header: "#dedede",
+    description: "Void",
+  },
+};
+//The function given to the button for updating the theme.
+function onThemeButtonClick() {
+  let theme = themes[document.getElementById("theme-picker").value];
+  document.body.style.backgroundColor = theme.body;
+  document.body.style.color = theme.text;
+  headingDiv.style.backgroundColor = theme.header;
+  
+  alert(theme.description);
+}
+//Gives the button the onThemeButtonClick function.
+themeButton.addEventListener("click", onThemeButtonClick);
+//OTHER STUFF
+// all the elements :)
+let convertButton = document.getElementById("convertButton"),
+  output = document.getElementById("results"),
+  snackTo = document.getElementById("snackTo"),
+  snackFrom = document.getElementById("snackFrom"),
+  amount = document.getElementById("amount");
+// object for amounts
+const amounts = {
+  oreo: {
+    amount: 1, // oreo is the baseline
+  },
+  taki: {
+    amount: 3, // takis are three times as much as oreos
+  },
+  twinkie: {
+    amount: 0.2, // and so on ...
+  },
+  welch: {
+    amount: 0.2,
+    name: "Welch's Fruit Gummies",
+    unit: " (in packet(s))",
+  },
+};
+// rounding function because computers are bad at decimal math
+function round(n, precision) {
+  precision = Math.pow(10, precision);
+  return Math.round(n * precision) / precision;
+}
+//function for convert
+var convert = function () {
+  let amountNum = parseInt(amount.value),
+    to = snackTo.value,
+    from = snackFrom.value;
+  if (from === to) return "Look, they are the same amount.";
 
-        convertButton.addEventListener("click", onConvertButtonClick);
+  // if there is a unit, the unit. if there isn't, (s)
+  let fromUnit = amounts[from].unit || "(s)";
+  // you get the idea
+  let toUnit = amounts[to].unit || "(s)";
+  let fromName = amounts[from].name || from;
+  let toName = amounts[to].name || to;
+
+  // calculate amount
+  let newAmount = amounts[from].amount * amountNum * amounts[to].amount;
+  // round it because 0.1 + 0.2 = 0.30000000000000004
+  // in other words, computers are bad at math
+  newAmount = round(newAmount, 2);
+  
+    // if input is not a number
+    if (isNaN(amountNum)) return `How can you have "${amount.value}" ${fromName}${fromUnit}?!`;
+
+  // return the output
+  return `${amountNum} ${fromName}${fromUnit} is/are equal to ${newAmount} ${toName}${toUnit}`;
+};
+
+convertButton.addEventListener("click", function () {
+  output.innerHTML = convert();
+});
